6642374b66dba4a70efb57a180d8b782d5a70333
tigerfyoficial <tigerfyoficial@outlook.com>
2025-11-14 05:44:02 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // api/index.js
 const serverless = require('serverless-http');
 process.env.VERCEL = '1';
-const app = require('../server'); // seu Express
+const app = require('../server');
 module.exports = serverless(app);
