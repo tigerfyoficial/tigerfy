035ad27da014ffd5a4d477f81a5a7de244c7364b
tigerfyoficial <tigerfyoficial@outlook.com>
2025-11-14 05:12:04 -0300

    Delete api directory

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-process.env.VERCEL = '1';        // garante export do app, não dá listen
-const app = require('../server'); // usa seu server.js (sem tocar no front)
-module.exports = serverless(app);
