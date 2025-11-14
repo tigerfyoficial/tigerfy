95bd32bf0f17e9403ea0c136d2aadc4066d7c343
tigerfyoficial <tigerfyoficial@outlook.com>
2025-11-14 05:22:02 -0300

    Update Bot.js

--- models/Bot.js
+++ models/Bot.js
@@ -1,12 +1,3 @@
-const mongoose = require("mongoose");
-
-const BotSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    token: { type: String, required: true },
-    note: { type: String, default: "" }
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Bot", BotSchema);
+const { makeMemModel } = require("./_mem");
+const Bot = makeMemModel([]);
+module.exports = Bot;
