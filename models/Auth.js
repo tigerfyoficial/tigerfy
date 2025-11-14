0e0e29790aae3af0d7c8ce481a41006dae8ffd90
tigerfyoficial <tigerfyoficial@outlook.com>
2025-11-14 05:22:14 -0300

    Update Auth.js

--- models/Auth.js
+++ models/Auth.js
@@ -1,12 +1,3 @@
-const mongoose = require("mongoose");
-
-const AuthSchema = new mongoose.Schema(
-  {
-    adminId: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-    token: String,
-    expiresAt: Date
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Auth", AuthSchema);
+const { makeMemModel } = require("./_mem");
+const Auth = makeMemModel([]);
+module.exports = Auth;
